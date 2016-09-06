824cce976e7383aca0d7c04218bca038dfcb9efa
Timo Tuononen <tixtixti@cs.helsinki.fi>
2016-09-06 22:53:52 +0300

    landing page

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './Index.jsx',
+  entry: './src/Index.jsx',
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
